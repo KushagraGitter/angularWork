1af0ff3873aedf2fe46f3b9110ae71c1149c8be7
KushagraGitter <kushagra305@gmail.com>
2017-07-06 01:21:03 +0530

    2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 (function(){
-    window.onload(function(){
+    window.onload = function(){
         console.log("hello");
-    })
+        var objFoo = new Foo();
+        document.getElementById("mybutton").addEventListener("click", (function(event){
+            console.log(this);
+        }).bind(Foo))
+        function Foo(){
+            var name = "appleName"
+            this.name = "apple";
+        }
+        
+    }
+  
 
 })();
